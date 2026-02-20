b6aa724490183e6985237bb942f68e8384a62777
abuXahra <abdulmuminisah@yahoo.com>
2026-02-20 12:11:46 +0100

    Add program that print a line of text without using printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,15 @@
-#include <stdio.h>#include <stdio.h>
+#include <unistd.h>
+
+/*
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: C code that print a line of text without using prinf and puts function
+ * */
+
+int main(void)
+{
+    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, str, 59);
+
+    return (1);
+}
