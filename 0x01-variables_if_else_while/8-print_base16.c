edda4b1eebd60253d1bfad150d0cd00ffb921d22
abuXahra <abdulmuminisah@yahoo.com>
2026-02-25 15:02:32 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Print the lowercase alphabets in revers order.
+ *
+ **/
+
+int main(void){
+
+    int i;
+
+    for(i = 0; i <= 15;  i++){
+    	printf("%x", i);
+    }
+
+    return 0;
+
+
+}
+
