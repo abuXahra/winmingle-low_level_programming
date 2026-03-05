35d729b4e2615061b6d8d1c234a069c7965ef407
abuXahra <abdulmuminisah@yahoo.com>
2026-03-05 15:36:45 +0100

    Added 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a program that prints _putchar, followed by a new line.
+ * */
+
+int main()
+{
+    printf("_putchar\n");
+    return (0);
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # This is WinMingle Low Levele Programing
 # 0x00-hello_word: 
 # 0x01-variables_if_else_while
+# 0X02-functions_nested_loops
 
 
