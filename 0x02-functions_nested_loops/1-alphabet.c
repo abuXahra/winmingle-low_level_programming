fe1b4350e0d81a2fa45c639d97fddbc4b41c86a9
abuXahra <abdulmuminisah@yahoo.com>
2026-03-05 15:57:45 +0100

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a functions that prints alphabet, in lowercase, followed by a new line.
+ * */
+
+int main()
+{
+    char i;
+    for(i ='a'; i <= 'z'; i++){
+    	putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
