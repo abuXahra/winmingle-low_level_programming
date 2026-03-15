f53bc1cb3ce974c0166532837b383b0b129211c6
abuXahra <abdulmuminisah@yahoo.com>
2026-03-15 14:08:43 +0100

    Added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include <ctype.h>
+
+int _islower(char c)
+{
+     if(islower(c)){
+         return (1);
+     }else{
+         return (0);
+     }
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that checks for lowercase character
+ * 
+ * */
+
+
+
+int main(void)
+{
+   
+    int  r;
+
+    r = _islower('H');
+
+    _putchar(r + '0');
+
+   _putchar('\n');
+
+    return (0);
+    
+}
