09c8fa3ea8c678821cff7c0bfe75963c7575c21d
abuXahra <abdulmuminisah@yahoo.com>
2026-03-15 15:50:13 +0100

    Added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that computes the absolute value of an integer
+ *
+ * */
+
+
+
+int main(void)
+{
+   
+    int  r;
+
+    r = _abs(-1);
+    printf("%d\n", r);
+   
+    r = _abs(0);
+    printf("%d\n", r);
+       
+    r = _abs(1);
+    printf("%d\n", r);
+   
+    r = _abs(-98);
+    printf("%d\n", r);
+   
+
+   
+    return (0);
+    
+}
