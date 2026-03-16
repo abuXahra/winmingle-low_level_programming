9f3475696b985ba9767647cb92792f24745f1d31
abuXahra <abdulmuminisah@yahoo.com>
2026-03-16 15:41:09 +0100

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * */
+
+
+
+int main(void)
+{  
+    jack_bauer();
+    return (0);
+    
+}
