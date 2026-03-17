eb9f3e4aa91ae8a40511dab3b22d3ef2dede3971
abuXahra <abdulmuminisah@yahoo.com>
2026-03-17 09:39:32 +0100

    Added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that prints the 9 times table, starting with 0.
+ *
+ * */
+
+
+
+int main(void)
+{
+   
+    times_table();
+    return (0);
+    
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+
+#include "main.h"
+
+
+void times_table(void)
+{
+    int i, j, result;
+
+    for (i = 0; i <= 9; i++){
+
+        for(j = 0; j <= 9; j++){
+
+    	   result = i * j;
+
+	   if(j != 0)
+	   {
+	       _putchar(',');
+	       _putchar(' ');
+	
+	       if(result < 10)
+	       {
+	           _putchar(' ');
+	       }
+
+	   }
+
+	   if(result >= 10)
+	   {
+               _putchar((result / 10) + '0');
+	   }
+
+	   _putchar((result % 10) + '0');
+	
+	}
+        _putchar('\n');
+    }
+}
