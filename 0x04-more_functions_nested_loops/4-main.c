e6656671eabe5ce9ff0342c8fbc7c210956d9972
abuXahra <abdulmuminisah@yahoo.com>
2026-03-28 13:56:44 +0100

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that prints the number, from 0 to 9, followed by a new line.
+ * */
+
+int main(void)
+{
+    print_most_numbers();
+    return 0;   
+}
