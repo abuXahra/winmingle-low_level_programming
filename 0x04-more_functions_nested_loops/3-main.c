316af01c628cbf064922db959c3a1c13488f1c26
abuXahra <abdulmuminisah@yahoo.com>
2026-03-28 13:23:05 +0100

    Added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that prints the number, from 0 to 9, followed by a new line.
+ * */
+
+int main(void)
+{
+    print_numbers();
+    return 0;
+    
+}
