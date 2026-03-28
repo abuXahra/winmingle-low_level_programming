c58985ab0ca42e4f73e9e635a98f2be1f96d2edc
abuXahra <abdulmuminisah@yahoo.com>
2026-03-28 15:11:37 +0100

    Added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that draws a straight line in the terminal.
+ * */
+
+int main(void)
+{
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+
+    return 0;   
+}
