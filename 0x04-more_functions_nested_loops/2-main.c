1d1d76faa36e450b4712d18701ee4ea112b93749
abuXahra <abdulmuminisah@yahoo.com>
2026-03-28 12:43:42 +0100

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that multipies two integers.
+ * */
+
+int main(void)
+{
+    
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return 0;
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+
+
+int mul(int a, int b)
+{
+    int result;
+    result = a * b;
+    return result;
+}
+
+
