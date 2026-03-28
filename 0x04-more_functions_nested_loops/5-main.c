a0071b3d0531b6ab3b022b5a7bed992d96eff31a
abuXahra <abdulmuminisah@yahoo.com>
2026-03-28 14:51:42 +0100

    Added 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that prints the number, from 0 to 14, followed by a newine.
+ * */
+
+int main(void)
+{
+    more_numbers();
+    return 0;   
+}
