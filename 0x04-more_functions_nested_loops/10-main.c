d602cf7b555c97622323a90ea50c87d1711634d6
abuXahra <abdulmuminisah@yahoo.com>
2026-03-31 11:10:31 +0100

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that prints a triangle, followed by anew line 
+ * */
+
+int main(void)
+{
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+
+
+    return 0;   
+}
