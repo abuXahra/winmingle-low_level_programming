351a5c328c9664be8e8f6cb8b46c004127e0dae5
abuXahra <abdulmuminisah@yahoo.com>
2026-03-31 14:44:28 +0100

    Added 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Isah Abdulmumin
+ * Program: WinMingle Community C Training
+ * Description: Write a function that prints an integer.
+ * */
+
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+
+    return 0;   
+}
