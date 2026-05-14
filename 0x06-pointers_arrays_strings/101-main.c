939c86b07def9a91f7cbe333e0b56ec69cf0b893
abuXahra <abdulmuminisah@yahoo.com>
2026-05-14 12:24:52 +0100

    Added 101-print_number

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+
+/*
+ *  Author: Isah Abdulmumin
+ *  Program: WinMingle Community C Training
+ *  Description: Write a funtion that prints an integer using only _putchar.
+ * */
+
+
+
+int main(void)
+{
+    
+    print_number(98); 
+    _putchar('\n'); 
+    print_number(402); 
+    _putchar('\n'); 
+    print_number(1024); 
+    _putchar('\n'); 
+    print_number(0); 
+    _putchar('\n'); 
+    print_number(-98); 
+    _putchar('\n'); 
+
+    return 0;
+}
